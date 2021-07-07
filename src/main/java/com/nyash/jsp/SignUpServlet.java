72dfb7167117e0ece99a05329b1e96a43f8a0819
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-08 05:22:35 +0700

    add changes

--- src/main/java/com/nyash/jsp/SignUpServlet.java
+++ src/main/java/com/nyash/jsp/SignUpServlet.java
@@ -41,5 +41,7 @@ public class SignUpServlet extends HttpServlet {
         User user = new User(name, password,birthDate);
 
         usersRepository.save(user);
+
+        doGet(request,response);
     }
 }
