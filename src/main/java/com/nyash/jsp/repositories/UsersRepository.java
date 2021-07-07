ca6f8056bc62316ec26279dcd25613c8459ab6d3
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-08 02:37:28 +0700

    add changes

--- src/main/java/com/nyash/jsp/repositories/UsersRepository.java
+++ src/main/java/com/nyash/jsp/repositories/UsersRepository.java
@@ -2,26 +2,9 @@ package com.nyash.jsp.repositories;
 
 import com.nyash.jsp.models.User;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
-public class UsersRepository {
+public interface UsersRepository {
 
-    private List<User> users;
-
-    public UsersRepository () {
-        this.users = new ArrayList<>();
-        User user = new User("Alex", "qwerty", LocalDate.parse("1988-04-16"));
-        User user1 = new User("Mike", "12345", LocalDate.parse("1988-06-12"));
-        User user2 = new User("Tom", "qwerty123", LocalDate.parse("1999-05-11"));
-
-        users.add(user);
-        users.add(user1);
-        users.add(user2);
-    }
-
-    List<User> findAll() {
-        return this.users;
-    }
+    List<User> findAll();
 }
