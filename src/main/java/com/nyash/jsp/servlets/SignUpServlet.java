7e7e5de92e991271d7b14189a9302c1578a71218
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-13 03:43:48 +0700

    add changes

--- src/main/java/com/nyash/jsp/servlets/SignUpServlet.java
+++ src/main/java/com/nyash/jsp/servlets/SignUpServlet.java
@@ -1,47 +1,47 @@
-package com.nyash.jsp.servlets;
-
-import com.nyash.jsp.models.User;
-import com.nyash.jsp.repositories.UsersRepository;
-import com.nyash.jsp.repositories.UsersRepositoryInMemoryImpl;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.time.LocalDate;
-import java.util.List;
-
-@WebServlet("/signUp")
-public class SignUpServlet extends HttpServlet {
-
-    private UsersRepository usersRepository;
-
-    @Override
-    public void init() throws ServletException {
-        this.usersRepository = new UsersRepositoryInMemoryImpl();
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        List<User> users = usersRepository.findAll();
-        request.setAttribute("usersFromServer", users);
-        RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/jsp/Sign-Up.jsp");
-        dispatcher.forward(request, response);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String name = request.getParameter("name");
-        String password = request.getParameter("password");
-        LocalDate birthDate = LocalDate.parse(request.getParameter("birthDate"));
-
-        User user = new User(name, password,birthDate);
-
-        usersRepository.save(user);
-
-        doGet(request,response);
-    }
-}
+//package com.nyash.jsp.servlets;
+//
+//import com.nyash.jsp.models.User;
+//import com.nyash.jsp.repositories.UsersRepository;
+//import com.nyash.jsp.repositories.UsersRepositoryInMemoryImpl;
+//
+//import javax.servlet.RequestDispatcher;
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.time.LocalDate;
+//import java.util.List;
+//
+//@WebServlet("/signUp")
+//public class SignUpServlet extends HttpServlet {
+//
+//    private UsersRepository usersRepository;
+//
+//    @Override
+//    public void init() throws ServletException {
+//        this.usersRepository = new UsersRepositoryInMemoryImpl();
+//    }
+//
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        List<User> users = usersRepository.findAll();
+//        request.setAttribute("usersFromServer", users);
+//        RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/jsp/Sign-Up.jsp");
+//        dispatcher.forward(request, response);
+//    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        String name = request.getParameter("name");
+//        String password = request.getParameter("password");
+//        LocalDate birthDate = LocalDate.parse(request.getParameter("birthDate"));
+//
+//        User user = new User(name, password,birthDate);
+//
+//        usersRepository.save(user);
+//
+//        doGet(request,response);
+//    }
+//}
--- src/main/java/com/nyash/jsp/servlets/UsersServlet.java
+++ src/main/java/com/nyash/jsp/servlets/UsersServlet.java
@@ -6,9 +6,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
 
 @WebServlet("/users")
 public class UsersServlet extends HttpServlet {
+
+    private Connection connection;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
