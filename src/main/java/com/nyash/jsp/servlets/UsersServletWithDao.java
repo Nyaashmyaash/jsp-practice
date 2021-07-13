a3b167dbc78ae088dad10f19d4b0dd790948eaff
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-14 01:33:06 +0700

    add changes

--- src/main/java/com/nyash/jsp/servlets/UsersServletWithDao.java
+++ src/main/java/com/nyash/jsp/servlets/UsersServletWithDao.java
@@ -2,10 +2,19 @@ package com.nyash.jsp.servlets;
 
 import com.nyash.jsp.dao.UserDao;
 import com.nyash.jsp.dao.UserDaoJdbcImpl;
+import com.nyash.jsp.models.User;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Properties;
 
 @WebServlet("/users")
 public class UsersServletWithDao extends HttpServlet {
@@ -14,6 +23,31 @@ public class UsersServletWithDao extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        userDao = new UserDaoJdbcImpl()
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        Properties properties = new Properties();
+
+        try {
+            properties.load(new FileInputStream(getServletContext().getRealPath("/WEB-INF/classes/db.properties")));
+            String dbUrl = properties.getProperty("db.url");
+            String dbUserName = properties.getProperty("db.username");
+            String dbPassword = properties.getProperty("db.password");
+            String driverClassName = properties.getProperty("db.driverClassName");
+
+            dataSource.setUsername(dbUserName);
+            dataSource.setPassword(dbPassword);
+            dataSource.setUrl(dbUrl);
+            dataSource.setDriverClassName(driverClassName);
+
+            userDao = new UserDaoJdbcImpl(dataSource);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<User> users = userDao.findAll();
+        req.setAttribute("usersFromServer", users);
+        req.getServletContext().getRequestDispatcher("/jsp/addUser.jsp").forward(req, resp);
     }
 }
